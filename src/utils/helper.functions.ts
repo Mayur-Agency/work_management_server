5b3678190ad97a211c4c0e828b6608765df5cbd2
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2023-02-22 01:41:11 +0530

    created custom isEmpty and port change

--- src/utils/helper.functions.ts
+++ src/utils/helper.functions.ts
@@ -12,3 +12,19 @@ export function parseBoolean(value: string): boolean {
       throw new Error("Invalid boolean value");
   }
 }
+
+export function isEmpty(value: any): boolean {
+  if (value === undefined || value === null) {
+    return true;
+  }
+
+  if (typeof value === 'string' || Array.isArray(value)) {
+    return value.length === 0;
+  }
+
+  if (typeof value === 'object') {
+    return Object.keys(value).length === 0;
+  }
+
+  return false;
+}
